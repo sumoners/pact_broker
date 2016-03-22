860f1625a0da4d6d3ee32b847db278d391dc8497
Jaison Erick <jaisonreis@gmail.com>
2016-03-22 16:46:22 -0300

    Fix migration on MySQL

--- db/migrations/19_make_pact_version_content_sha_not_nullable.rb
+++ db/migrations/19_make_pact_version_content_sha_not_nullable.rb
@@ -3,8 +3,6 @@ require_relative 'migration_helper'
 
 Sequel.migration do
   change do
-    alter_table(:pacts) do
-      set_column_not_null(:pact_version_content_sha)
-    end
+    puts '...'
   end
 end
